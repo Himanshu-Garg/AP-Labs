d76f015b03ce179e8d74ab85846d970cb3b3abff
himanshu <himanshu@himanshus-MacBook-Pro.local>
2019-08-11 18:54:38 +0530

    .

--- lab1/src/lab1.java
+++ lab1/src/lab1.java
@@ -59,6 +59,7 @@ public class lab1 {
             this.rollno = rollno;
             this.cgpa = cgpa;
             this.course = course;
+            scores.add(0);      // score at index 0 is used during sorting...
         }
 
         // for adding marks to the student object
@@ -75,6 +76,8 @@ public class lab1 {
             if(getPlacement_status().equals("Placed")) {
                 System.out.println("Company at which placed : "+getCompany_placed());
             }
+
+            //System.out.println("scores : "+ Arrays.toString(new List[]{scores}));
         }
 
         // for sorting (creating comparator)
@@ -151,15 +154,14 @@ public class lab1 {
     }
 
     public static class Application {
-        private List<student> Students;
-        private List<company> Companies_left;
+        private List<student> Students = new ArrayList<>();;
+        private List<company> Companies_left = new ArrayList<>();
         private List<String> all_companies_visited = new ArrayList<>();
         private int no_of_students;
 
         Application() {
-            Students = new ArrayList<>();
-            Companies_left = new ArrayList<>();
             no_of_students = 0;
+            all_companies_visited.add("for sorting");       // since 0th index is reserved for sorting
         }
 
         // getter functions
@@ -206,9 +208,11 @@ public class lab1 {
             all_companies_visited.add(name);
 
             System.out.println("Enter Scores for Technical round : ");
+            boolean atleast_one_eligible = false;
             Scanner scan = new Scanner(System.in);
             for(int i=0;i<Students.size();i++) {
                 if(course_criteria.contains(Students.get(i).course)) {
+                    atleast_one_eligible = true;
                     System.out.println("Enter score for Roll no. "+Students.get(i).getRollno());
                     int s = scan.nextInt();
                     Students.get(i).scores.add(s);
@@ -216,6 +220,9 @@ public class lab1 {
                 else {
                     Students.get(i).scores.add(-1);
                 }
+            }
+            if(atleast_one_eligible==false) {
+                System.out.println("SORRY - no student eligible for technical round ...");
             }
         }
 
@@ -228,7 +235,7 @@ public class lab1 {
         private List<student> find_eligible_students(String _company_name) {
 
             int company_index = 0;
-            for (int i=0;i<all_companies_visited.size();i++) {
+            for (int i=1;i<all_companies_visited.size();i++) {
                 if(all_companies_visited.get(i).equals(_company_name)) {
                     company_index = i;
                     break;
@@ -329,11 +336,19 @@ public class lab1 {
 
         public void display_select_students (String _company_name) {
             List<Integer> selected_students = select_students(_company_name);
+
             if(selected_students!=null) {
-                System.out.println("Roll number of Selected Students : ");
-                for (int i=0;i<selected_students.size();i++) {
-                    System.out.println(selected_students.get(i));
+                if(selected_students.size()==0) {
+                    System.out.println("NO student selected ...");
                 }
+                else {
+                    System.out.println("Roll number of Selected Students : ");
+                    for (int i=0;i<selected_students.size();i++) {
+                        System.out.println(selected_students.get(i));
+                    }
+                }
+
+
             }
 
             if(Students.size()<=0) {
@@ -439,16 +454,22 @@ public class lab1 {
 
         public void companies_applied_by_student(int _roll_no) {
             student s = return_student_by_rollno(_roll_no);
+            boolean atleast_one_test_given = false;
             if(s==null) {
                 System.out.println("Student not in database ... ");
             }
             else {
-                for(int i=0;i<s.scores.size();i++) {
+                for(int i=1;i<s.scores.size();i++) {
                     if(s.scores.get(i)!=-1) {
                         System.out.println(all_companies_visited.get(i) +" " + s.scores.get(i));
+                        atleast_one_test_given = true;
                     }
                 }
+                if(atleast_one_test_given == false) {
+                    System.out.println("NOT eligible for technical round of any company");
+                }
             }
+
         }
 
 
@@ -469,11 +490,15 @@ public class lab1 {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int no_of_students = scan.nextInt();
 
+            if(no_of_students<=0) {
+                System.out.println("Exiting ... ");
+                System.exit(0);
+            }
+
             Application app = new Application();
 
             for(int i=0;i<no_of_students;i++) {
                 String[] inp = br.readLine().trim().split("\\s+");
-
                 float cgpa = Float.parseFloat(inp[0]);
                 app.add_student(cgpa, inp[1]);
             }
